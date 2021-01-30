b5be41af84f4d5e2b8dc377b2322b5bda940df51
rabiulislamrubel <robiul91912113@gmail.com>
2021-01-30 16:06:57 +0600

    assignment 4

---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById('firstClass-increase-btn').addEventListener('click', function(){
+    var firstClassTicketPrice = IncreaseDecreaseBtn(true, 'firstClass-ticket-count', 150);
+    document.getElementById('f-per-ticket-price').innerText = firstClassTicketPrice;
+    totalCalculation();
+});
+document.getElementById('firstClass-decrease-btn').addEventListener('click', function(){
+    var firstClassTicketPrice = IncreaseDecreaseBtn(false, 'firstClass-ticket-count', 150);
+    document.getElementById('f-per-ticket-price').innerText = firstClassTicketPrice;
+    totalCalculation();
+});
+document.getElementById('economy-increase-btn').addEventListener('click', function(){
+    var economyTicketPrice = IncreaseDecreaseBtn(true, 'economy-ticket-count', 100);
+    document.getElementById('e-per-ticket-price').innerText = economyTicketPrice;
+    totalCalculation();
+});
+document.getElementById('economy-decrease-btn').addEventListener('click', function(){
+    var economyTicketPrice = IncreaseDecreaseBtn(false, 'economy-ticket-count', 100);
+    document.getElementById('e-per-ticket-price').innerText = economyTicketPrice;
+    totalCalculation();
+})
+
+function IncreaseDecreaseBtn(plusBtn,id,price){
+    const ticketCount = document.getElementById(id);
+    const ticketCountNum = parseInt(ticketCount.value);
+    let ticketNewCount;
+    if(plusBtn == true){
+        ticketNewCount = ticketCountNum + 1;
+    }
+    else if(plusBtn == false){
+        ticketNewCount = ticketCountNum - 1;
+    }
+    if(ticketNewCount < 0){
+        ticketNewCount = 0;
+    }
+    document.getElementById(id).value = ticketNewCount;
+    const ticketPrice = ticketNewCount * price;
+    return ticketPrice;
+}
+
+function totalCalculation(){
+    const firstClassTotal = document.getElementById('f-per-ticket-price');
+    const firstClassTotalNum = parseInt(firstClassTotal.innerText);
+    const economyTotal = document.getElementById('e-per-ticket-price');
+    const economyTotalNum = parseInt(economyTotal.innerText);
+
+    let subTotal = firstClassTotalNum + economyTotalNum;
+    document.getElementById('sub-total').innerText = '$'+ subTotal;
+    let tax = subTotal * 10 / 100;
+    document.getElementById('tax').innerText = '$' + tax;
+
+    let total = subTotal + tax;
+    document.getElementById('total').innerText = '$'+ total;
+}
